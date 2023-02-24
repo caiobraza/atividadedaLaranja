1ec4844305535362ceea798b8e49f401802efc91
caiobraza <caio.lg.g2@gmail.com>
2023-02-24 20:14:11 -0300

    first commit

---
+++ atividade/laranja.java
@@ -0,0 +1,19 @@
+package atividade;
+
+public class laranja {
+
+	public static void main(String[] args) {
+	
+		/* João tem 60 laranjas e gostaria de dividi-las em quantidades
+		 * iguais com seus amigos
+		 * com Mateus, Pedro e Lucas.
+		*/
+		
+		int amigos = 4;
+		int laranjas = 60;
+		int resultado = laranjas / amigos;
+		
+		System.out.println(" A quantidade de laranjas é : " + resultado);
+	}
+
+}
